e48f6ccf8c8fcac901a091d296e898398d1a690b
Vinayak MK <48713290+VinayakMK@users.noreply.github.com>
2023-09-21 00:10:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+//let ele = document.getElementById("heading1").parentNode;
+
+//let node = document.getElementById("heading1").childNodes[0];
+
+let node = document.getElementById("body").childNodes[0];
+
+console.log(node)
